38d2d0157450809de9c6bf2af9e9dbdc977ea13b
Warnar Boekkooi <warnar@boekkooi.net>
2015-01-06 11:34:02 +0100

    Fixed tests. For some reason \PHPUnit_Framework_Error_Deprecated::$enabled stopped working correctly after a SF update so let's not use it.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,3 @@ EOT
 }
 
 \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
-if (class_exists('PHPUnit_Framework_Error_Deprecated')) {
-    // Thanks to https://github.com/symfony/symfony/issues/12710 we need to add the following:
-    \PHPUnit_Framework_Error_Deprecated::$enabled = false;
-}
